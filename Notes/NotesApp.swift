1dedbfe9be7e20177a75da057cea9fad7b683083
Dharsini <dharsinis2000@gmail.com>
2025-09-11 11:22:39 +0530

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Dharsini S on 11/09/25.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
